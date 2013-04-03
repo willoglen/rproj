8548665d3f463c014ad34371a677e70f869e96d2
Glen Cunningham <glencunningham@citizenschools.org>
2013-04-03 12:09:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rproj::Application.config.secret_token = '06e7ae0a829f8473be1dd971c26ed31a7f92406d7f9bb07ba8312a69f824cd4c1b5260c49a33357d1b55b7f04753b29c983ab8df0f06718f034e1db88f11afaa'
